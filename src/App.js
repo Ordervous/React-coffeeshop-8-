ecc61802aa0cf79d601c10a9abc09b9ffbfe4c19
Brett <bdmdiving@gmail.com>
2022-07-21 07:08:11 +1000

    Index & App files done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+      <Route index element={<h1>Welcome</h1>} />
+          <Route path="contacts" element={<ProductList />} >
+            <Route index element={<p>Select a product for more details</p>} />
+            <Route path="new" element={<NewProduct />} />
+            <Route path=":productId/edit" element={<EditProduct />} />
+            <Route path=":productId" element={<ProductDetail />} />
+            <Route path="*" element={<h1>Productct Not Found</h1>} />
+          </Route>
+      </Route>
+      <Route path="*" element={<h1>Page Not Found</h1>} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
