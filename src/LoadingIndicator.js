3b826dcd78aed20ea37cdbd53e30e2f4c8736088
Brett <bdmdiving@gmail.com>
2022-07-21 08:22:33 +1000

    fixed list all

---
+++ src/LoadingIndicator.js
@@ -0,0 +1,30 @@
+import Spinner from 'react-bootstrap/Spinner'
+import { useState } from 'react'
+import axios from 'axios'
+
+function LoadingIndicator() {
+  let [ visible, setVisible ] = useState(false)
+
+  // Global Interceptors - request interceptor (to make it visible when the app begins a request)
+  axios.interceptors.request.use((config) => {
+    setVisible(true)
+    return config
+  }) 
+  axios.interceptors.response.use((response) => {
+    setVisible(false)
+    return response;
+  }, (error) => {
+    setVisible(false)
+    return Promise.reject(error);
+  }) 
+
+  if (!visible) return null
+  return (
+    <Spinner animation="grow" size="sm" variant="light" />
+  )
+
+  
+
+}
+
+export default LoadingIndicator
