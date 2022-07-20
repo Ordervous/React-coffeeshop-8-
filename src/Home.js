44abdfb0360623f1f412dc94bc13e18488257ff6
Brett <bdmdiving@gmail.com>
2022-07-21 07:12:33 +1000

    Home set with Navbar and Loading

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+import { Link, Outlet } from "react-router-dom"
+import LoadingIndicator from './LoadingIndicator'
+
+function Home() {
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Nav className="me-auto">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/products" className="nav-link">Products</Link>
+                        <Link to="/products/new" className="nav-link">New Product</Link>
+                    </Nav>
+                    <Navbar.Text>
+                        <LoadingIndicator />
+                    </Navbar.Text>
+                </Container>
+            </Navbar>
+
+            <Stack gap={3} className="col-md-10 mx-auto mt-3">
+                <Outlet />
+            </Stack>
+        </>
+    )
+}
+
+export default Home
