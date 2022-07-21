6b63a017e66ad30fbc13c6e91df2564e23ebb978
Brett <bdmdiving@gmail.com>
2022-07-21 19:56:53 +1000

    added form for new product with image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home'
 import ProductList from './ProductList'
-import NewProduct from './NewProduct'
-import EditProduct from './EditProduct'
+import ProductForm from './ProductForm'
 import ProductDetail from './ProductDetail'
 
 
@@ -15,8 +14,8 @@ function App() {
       <Route index element={<h1>Welcome</h1>} />
           <Route path="products" element={<ProductList />} >
             <Route index element={<p>Select a product for more details</p>} />
-            <Route path="new" element={<NewProduct />} />
-            <Route path=":productId/edit" element={<EditProduct />} />
+            <Route path="new" element={<ProductForm />} />
+            <Route path=":productId/edit" element={<ProductForm />} />
             <Route path=":productId" element={<ProductDetail />} />
             <Route path="*" element={<h1>Productct Not Found</h1>} />
           </Route>
